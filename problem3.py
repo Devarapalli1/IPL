b4e532b9dc8813a40a73de161cb7f3290e01781d
vamsi <d.n.s.k.vamsi@gmail.com>
2021-11-10 15:56:17 +0530

    problem3

---
+++ problem3.py
@@ -0,0 +1,34 @@
+import csv
+
+import matplotlib.pyplot as plt
+
+u = []
+with open(r"/home/ubuntu/project1/umpires.csv", "r") as k:
+    read = csv.reader(k)
+    for i in read:
+        u.append(i)
+
+
+no_of_umpires_by_country = {}
+
+
+for i in range(1, len(u)):
+    if u[i][1] in no_of_umpires_by_country:
+        no_of_umpires_by_country[u[i][1]] += 1  ##  0 -'umpire', 1 - ' country'
+    else:
+        no_of_umpires_by_country[u[i][1]] = 1
+
+# Removing india
+del no_of_umpires_by_country[" India"]
+
+
+country = []
+count = []
+for i, j in no_of_umpires_by_country.items():
+    country.append(i)
+    count.append(j)
+
+
+plt.figure(figsize=(10, 10))
+plt.plot(country, count)
+plt.show()
